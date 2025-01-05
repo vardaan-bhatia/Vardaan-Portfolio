08f3321376c78c45cc6105ff1c05ed8f2c504b70
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2025-01-05 16:57:31 +0530

    remove vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -65,7 +64,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             {children}
-            <Analytics />
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
