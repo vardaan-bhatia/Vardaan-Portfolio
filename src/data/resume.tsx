f76d0419c182efab564b55ff968daa55a63a92a4
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2025-04-05 20:55:25 +0530

    final logo flipkart

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -77,7 +77,7 @@ export const DATA = {
       badges: [],
       location: "Bengaluru",
       title: "SDE-UI",
-      logoUrl: "/public/20241212055950_flipkart.avif",
+      logoUrl: "/flip.png",
       start: "March 2025",
       end: "Present",
       description: "Flipkart data platform team via Thinkify Labs",
